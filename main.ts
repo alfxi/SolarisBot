cfcf1f3756b9fc42ed78ee4ec7acaf298dc3c736
alfxi <93888045+alfxi@users.noreply.github.com>
2025-04-10 06:30:21 +0300

    yes

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ import { Bot } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 import { TwitterApi } from "https://esm.sh/twitter-api-v2@1.12.5";
 
 const bot = new Bot({
-  token: Deno.env.get("DISCORD_TOKEN")!,
-  intents: ["Guilds", "GuildMessages"],
+    token: Deno.env.get("DISCORD_TOKEN")!,
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
 const twitter = new TwitterApi(Deno.env.get("TWITTER_BEARER_TOKEN")!);
 let lastTweetId = "";
 
-bot.events.ready = () => {
+bot.events.ready = () => console.log("Bot is online!");
   setInterval(async () => {
     try {
       const timeline = await twitter.v2.userTimeline(
